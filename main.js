07f1db2632ed279843d5b1059e8606bfbd9a513e
Mike Dauphinais <mdauphinais@voxbone.com>
2016-04-10 13:27:19 +0200

    client

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-/**
- * Created by mdauphinais on 10.04.16.
- */
 
 
 var ngrok = require('ngrok');
+var chokidar = require('chokidar');
+var simpleGit = require('simple-git')();
+var Client = require('node-rest-client').Client;
+var express = require('express');
+var restApp = express();
+var bodyParser = require('body-parser');
 
 var roomId = 'Y2lzY29zcGFyazovL3VzL1JPT00vMDA2ZTdiMjAtZmQxMS0xMWU1LTg2NDUtOWZjNGE1MmNmMGQ5';
 var client = new Client();
@@ -17,10 +20,33 @@ var args = {
     }
 };
 
+chokidar.watch('.', {ignored: /^\.idea|^\.git/gm}).on('change', function (path, event) {
+    simpleGit.diffSummary(function (error, response) {
+        var files = response.files.map(function (file) {
+            return file.file
+        });
+
+        var changes = {
+            user: user,
+            files: files
+        };
+
+        console.log('Sending new changes...');
+        args.data = {
+            'roomId': roomId,
+            'text': JSON.stringify(changes)
+        };
+
+        client.post('https://api.ciscospark.com/v1/messages', args, function (data, response) {
+            console.log('Message sent: ', response.statusCode);
+        });
+    });
+});
+
 ngrok.connect(
     {
         proto: 'http',
-        addr: 3033
+        addr: 5055
     },
     function (err, url) {
         args.data = {
@@ -39,10 +65,16 @@ ngrok.connect(
             restApp.post('/', function (req, res) {
                 getMessageFromCisco(req.body.data.id);
             });
-            restApp.get('/merge', function (req, res) {
-                res.send(mergeConflicts);
-            });
-            restApp.listen(3033);
-            console.log('Listening on port 3033...');
+            restApp.listen(5055);
+            console.log('Listening on port 5055...');
         });
     });
+
+var getMessageFromCisco = function (messageId) {
+    client.get('https://api.ciscospark.com/v1/messages/'.concat(messageId), args, function (data, response) {
+        var receivedChanges = JSON.parse(data.text);
+        console.log(receivedChanges);
+    })
+};
+
+
